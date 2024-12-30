742bb1fe8877bfb053a714588d2f04227732a91e
DST\LungaM <Lunga.Mzobe@dst.gov.za>
2024-12-30 10:32:40 +0200

    first commit

---
+++ api/Program.cs
@@ -0,0 +1,44 @@
+using api.Data;
+using api.Interfaces;
+using api.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Options;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+// Database
+builder.Services.AddDbContext<ApplicationDBContext>( Options =>{
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(Options => {
+        Options
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
+            .WithTheme(ScalarTheme.BluePlanet);
+    });
+    // app.UseHsts();
+}
+
+// app.UseHttpsRedirection();
+
+// app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
